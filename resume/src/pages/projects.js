cb7528558d3da70539fdb04fce45b08682c97b79
dustin schober <schobed2@gmatc.matc.edu>
2024-02-19 21:07:20 -0600

    Contenful Added

---
+++ resume/src/pages/projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { graphql, Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
+
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+const Projects = ({ data }) => {
+    
+}
+
+export default Projects
